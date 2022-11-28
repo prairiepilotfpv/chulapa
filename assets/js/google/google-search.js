ae51e8c7ea37dbc9965edf3131d81e75986f033d
dieghernan <dieghernan@users.noreply.github.com>
2022-11-28 16:43:48 +0000

    jekyll build from Action d9670a16ce00095e0f485cea9a606d431e74b701

--- assets/js/google/google-search.js
+++ assets/js/google/google-search.js
@@ -1,7 +1,6 @@
 (function() {
     var cx = '002514516175957445995:tvgnhtemtbu';
     var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
     gcse.async = true;
     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
     var s = document.getElementsByTagName('script')[0];
